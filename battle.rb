692f489a0b56215f78e481e993c1d99e9ed00244
atsushi0919 <hatake0919@yahoo.co.jp>
2021-01-21 07:58:45 +0900

    メッセージの誤記修正

--- battle.rb
+++ battle.rb
@@ -1,12 +1,13 @@
 class Battle
   @@phase = 0
+
   def initialize(battle_table)
     @characters = [battle_table[:player], battle_table[:monster]]
     @member = @characters.size
   end
 
   def start
-    puts "#{@characters[-1].name} があわられた！"
+    puts "#{@characters[-1].name} があらわれた！"
     sleep 0.5
 
     # 戦闘開始
@@ -14,10 +15,10 @@ class Battle
       attacker_idx = @@phase % @member
       target_idx = (@@phase + 1) % @member
       @characters[attacker_idx].attack(@characters[target_idx])
-      
+
       # 双方の攻撃が終わったとき、またはターゲットが倒れた場合HPを表示する
       if @@phase % 2 == 1 or @characters[target_idx].hp <= 0
-        line ="*=*=*=*=*=*=*=*=*=*=*"
+        line = "*=*=*=*=*=*=*=*=*=*=*"
         puts line
         @characters.each do |character|
           puts "【#{character.name}】HP: #{character.hp}"
@@ -29,7 +30,7 @@ class Battle
       if @characters[target_idx].hp <= 0
         ending(attacker_idx)
         break
-      end  
+      end
       @@phase += 1
     end
   end
@@ -40,7 +41,7 @@ class Battle
     # 勇者の勝ち
     if winner_idx == 0
       puts "#{@characters[winner_idx].name} は #{@characters[(winner_idx + 1) % @member].name} をやっつけた！！"
-    # 勇者の負け
+      # 勇者の負け
     else
       puts "\e[31m#{@characters[0].name} は倒されてしまった！\e[0m"
     end
