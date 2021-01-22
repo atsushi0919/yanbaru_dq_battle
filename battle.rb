a2b5e1ab3255deef4ffb686051db2da15eea2b09
atsushi0919 <hatake0919@yahoo.co.jp>
2021-01-22 09:35:20 +0900

    Battleクラスの修正

--- battle.rb
+++ battle.rb
@@ -1,60 +1,63 @@
 class Battle
-  def initialize(battle_member)
-    @battle_member = battle_member
-    # 味方データ
-    @ally_leader = battle_member.find {|member| member.ally}.name
-    @ally_count = battle_member.select { |member| member.ally }.size
-    # 敵データ
-    @enemy_leader = battle_member.find {|member| !member.ally}.name
-    @enemy_count = battle_member.select { |member| !member.ally }.size 
+  MESSAGE_SPEED = 0.5
+  MESSAGE_LINE = "*" + "=*" * 14
+
+  def initialize(battle_members)
+    # バトルメンバー
+    @battle_members = battle_members
+    # 仲間の情報: リーダー名, 人数
+    @allies_leader = battle_members.find { |member| member.ally }.name
+    @allies_count = battle_members.count { |member| member.ally }
+    # 敵の情報: リーダー名, 人数
+    @enemies_leader = battle_members.find { |member| !member.ally }.name
+    @enemies_count = battle_members.count { |member| !member.ally }
   end
 
-  def start
-    puts "#{@characters[-1].name} があらわれた！"
-    sleep 0.5
-
-    # 戦闘開始
-    while true
-      attacker_idx = @@phase % @member
-      target_idx = (@@phase + 1) % @member
-      @characters[attacker_idx].attack(@characters[target_idx])
-
-      # 双方の攻撃が終わったとき、またはターゲットが倒れた場合HPを表示する
-      if @@phase % 2 == 1 or @characters[target_idx].hp <= 0
-        line = "*=*=*=*=*=*=*=*=*=*=*"
-        puts line
-        @characters.each do |character|
-          puts "【#{character.name}】HP: #{character.hp}"
-        end
-        puts line
-        sleep 0.5
-      end
-
-      # ターゲットがノックダウンならバトルを終了してループを抜ける
-      if @characters[target_idx].hp <= 0
-        ending(attacker_idx)
-        break
-      end
-      @@phase += 1
+  # バトルメンバーのステータスを表示
+  def show_members_hp
+    puts MESSAGE_LINE
+    (get_allies + get_enemies).each do |member|
+      puts "【#{member.name}】HP: #{member.hp}"
     end
+    puts MESSAGE_LINE
   end
 
-  def ending(winner_idx)
-    # 改行
-    puts
-    # 勇者の勝ち
-    if winner_idx == 0
-      puts "#{@characters[winner_idx].name} は #{@characters[(winner_idx + 1) % @member].name} をやっつけた！！"
-      # 勇者の負け
-    else
-      puts "\e[31m#{@characters[0].name} は倒されてしまった！\e[0m"
+  private
+
+  # 仲間リストを返す
+  def get_allies
+    @battle_members.select { |member| member.ally }
+  end
+
+  # 敵リストを返す
+  def get_enemies
+    @battle_members.select { |member| !member.ally }
+  end
+
+  # 開始メッセージを表示
+  def show_start_message
+    get_enemies.each do |enemy|
+      puts "#{enemy} があらわれた！"
+      sleep MESSAGE_SPEED
     end
-    # 改行
-    puts
   end
 end
 
 =begin
+def ending(winner_idx)
+  # 改行
+  puts
+  # 勇者の勝ち
+  if winner_idx == 0
+    puts "#{@characters[winner_idx].name} は #{@characters[(winner_idx + 1) % @member].name} をやっつけた！！"
+    # 勇者の負け
+  else
+    puts "\e[31m#{@characters[0].name} は倒されてしまった！\e[0m"
+  end
+  # 改行
+  puts
+end
+
 # メッセージ処理
     messages = ["#{@name} の攻撃！", "#{target.name} に"]
     messages[-1] += damage > 0 ? " #{damage} のダメージを与えた！" : "ダメージを与えられなかった！"
--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
 require "./brave"
 require "./monster"
 require "./battle"
+require "byebug"
 
 # キャラデータ
 MONSTER_LIST = [{ name: "メタルスライム", hp: 3, offense: 100, defense: 999 },
-                { name: "キメラ", hp: 180, offense: 130, defense: 100 }, 
+                { name: "キメラ", hp: 180, offense: 130, defense: 100 },
                 { name: "アークデーモン", hp: 210, offense: 140, defense: 80 },
                 { name: "りゅうおう", hp: 228, offense: 170, defense: 190 },
                 { name: "シドー", hp: 240, offense: 165, defense: 220 },
@@ -12,11 +13,13 @@ MONSTER_LIST = [{ name: "メタルスライム", hp: 3, offense: 100, defense: 9
 BRAVE = { name: "トンヌラ", hp: 230, offense: 170, defense: 180 }
 
 # 対戦メンバーを生成 (配列の先頭から順に攻撃する)
-battle_member = [ Brave.new(BRAVE),
+battle_members = [Brave.new(BRAVE),
                   Monster.new(MONSTER_LIST.sample),
-                  Monster.new(MONSTER_LIST.sample) ]
+                  Monster.new(MONSTER_LIST.sample)]
 
-# バトル開始 
-battle = Battle.new(battle_member)
+# バトル開始
+battle = Battle.new(battle_members)
+
+p battle
 
 #battle.start
